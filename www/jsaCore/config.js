b27423293e21660f2ce77e12aa664a28c0bfa064
lonce <cnmwll@nus.edu.sg>
2014-05-31 12:40:28 +0800

    trying to find sounrce of bluetooth bgu

--- www/jsaCore/config.js
+++ www/jsaCore/config.js
@@ -26,10 +26,10 @@ define(
 		exports.audioContext=null;
 		if('AudioContext' in window) {
     		exports.audioContext = new AudioContext();
-    		//alert("AudioContext OK!");
+    		alert("using normal AudioContext call ");
 		} else if ('webkitAudioContext' in window) {
     		exports.audioContext = new webkitAudioContext();
-    		//alert("AudioContext OK!");
+    		alert("using webkit AudioContext call");
 		}  else {
 			alert("Audio conext does not appear to be supported but we'll run silently anyway!");
 		    //throw new Error('AudioContext not supported. :(');
