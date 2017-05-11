460fd91fe9e6263ca4ca31eb11f46fa82e53301a
mclonce <lonce.wyse@nus.edu.sg>
2017-05-11 15:15:05 -0700

    microphone experiment

--- www/jsaSndLib/jsaOpCodes/jsaMicInputNode.js
+++ www/jsaSndLib/jsaOpCodes/jsaMicInputNode.js
@@ -10,34 +10,39 @@ define(
 
 				console.log("in gotAudio, stream is " + stream);
 				console.log("in gotAudio, audioContext is " + config.audioContext);
-				if (! config.microphone) {
-					config.microphone = config.audioContext.createMediaStreamSource(stream);
-					console.log("in gotAudio,  microphone is "  + microphone + ", and we are connecting to " + connect_to);
-				} else {
-					console.log("already have mic, use the same one.");
+
+				try{
+					if (! config.microphone) {
+						config.microphone = config.audioContext.createMediaStreamSource(stream);
+						console.log("in gotAudio,  microphone is "  + microphone + ", and we are connecting to " + connect_to);
+					} else {
+						console.log("already have mic, use the same one.");
+					}
+					microphone=config.microphone;
+
+					/* no need for this, and it doesn't solve am-noise problem
+	    			var splitter = config.audioContext.createChannelSplitter(2);
+	    			var merger = config.audioContext.createChannelMerger(2);
+
+	    			microphone.connect( splitter );
+	    			splitter.connect( merger, 0, 0 );
+	    			splitter.connect( merger, 0, 1 );
+	    			merger.connect(connect_to);
+					*/
+
+					/*
+					// do this GraphNode Wrapping just in case the node we are connecting to is a GraphNode
+					var micWrapper=microphone;
+					if (connect_to.nodeType==="GraphNode"){
+						micWrapper=GraphNode({}, [], [microphone]);
+					}
+					micWrapper.connect(connect_to);
+					*/
+
+					microphone.connect(connect_to);
+				} catch(e){
+					console.log('got stream, but not mic')
 				}
-				microphone=config.microphone;
-
-				/* no need for this, and it doesn't solve am-noise problem
-    			var splitter = config.audioContext.createChannelSplitter(2);
-    			var merger = config.audioContext.createChannelMerger(2);
-
-    			microphone.connect( splitter );
-    			splitter.connect( merger, 0, 0 );
-    			splitter.connect( merger, 0, 1 );
-    			merger.connect(connect_to);
-				*/
-
-				/*
-				// do this GraphNode Wrapping just in case the node we are connecting to is a GraphNode
-				var micWrapper=microphone;
-				if (connect_to.nodeType==="GraphNode"){
-					micWrapper=GraphNode({}, [], [microphone]);
-				}
-				micWrapper.connect(connect_to);
-				*/
-
-				microphone.connect(connect_to);
 
 
 			}
@@ -56,7 +61,7 @@ define(
 			}
 			*/
 
-/*----------------------
+
 			if (!navigator.getUserMedia)
 				navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
@@ -69,7 +74,7 @@ define(
 		            console.log('Error in getUserMedia');
 		            console.log(e);
 		        });
-*/
+
 
 		};
 	}
